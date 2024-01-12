44435272117558d2e14554bf82258c31905c9fcd
Collin Pfeifer <collinpfeifer@Icloud.com>
2024-01-12 14:58:55 -0500

    updated for not updated links

--- app/routes/api.stripe.buy.tsx
+++ app/routes/api.stripe.buy.tsx
@@ -2,6 +2,7 @@ import { redirect, json, ActionFunctionArgs } from '@remix-run/node';
 import { z } from 'zod';
 import { createCheckoutSession } from '../models/stripe.server';
 import invariant from 'tiny-invariant';
+import { getTicket } from '../models/ticket.server';
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = Object.fromEntries(await request.formData());
@@ -12,6 +13,9 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const result = buySchema.safeParse(formData);
   if (!result.success) return json({ errors: result.error.issues[0].message });
   const { ticketId, buyerUserId } = result.data;
+  const ticket = await (await getTicket({ id: ticketId })).json();
+  if (!ticket) return json({ errors: 'Ticket not found' });
+  if (ticket.sold) return json({ errors: 'Ticket already sold' });
   const session = await createCheckoutSession({
     ticketId,
     buyerUserId,
